145c6d23072c25f505c48163fc74bda273cf2814
pratham_srnk <pratham.sarankar@xtratrust.com>
2024-04-22 17:59:12 +0530

    Added LoadingContent.kt composable

--- app/src/main/java/sarankar/app/jetnewsapp/ui/screens/homescreen/HomeScreen.kt
+++ app/src/main/java/sarankar/app/jetnewsapp/ui/screens/homescreen/HomeScreen.kt
@@ -1,38 +1,66 @@
 package sarankar.app.jetnewsapp.ui.screens.homescreen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import sarankar.app.jetnewsapp.ui.screens.homescreen.components.LoadingContent
 
 @Composable
 fun HomeScreen(){
-    Scaffold {
-
-    }
+    HomeScreenScaffold(
+        showTopAppBar = true
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+@Preview
 fun HomeScreenScaffold(
     modifier: Modifier = Modifier,
-    showTopAppBar: Boolean,
+    showTopAppBar: Boolean = true,
 ){
     val topAppBarState = rememberTopAppBarState();
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior();
-    Scaffold(
-        topBar = {
-            if(showTopAppBar){
-                )
-            }
-        }
-    ) { innerPadding ->
-        val contentModifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
-
+    var isLoading by remember {
+        mutableStateOf(false)
     }
+    Scaffold {
+        Box(modifier = Modifier.padding(it)) {
+            LoadingContent(
+                empty = false,
+                emptyContent = { /*TODO*/ },
+                loading = isLoading,
+                onRefresh = { /*TODO*/ }) {
+                LazyColumn {
+                    items(20) {
+                        Card(
+                            modifier = Modifier.fillMaxWidth().height(100.dp).padding(8.dp),
+                        ) {
+                            Text(text = "Item $it")
+                        }
+                    }
+                }
+            }
 
+        }
+    }
 }
 
--- app/src/main/java/sarankar/app/jetnewsapp/ui/screens/homescreen/components/LoadingContent.kt
+++ app/src/main/java/sarankar/app/jetnewsapp/ui/screens/homescreen/components/LoadingContent.kt
@@ -1,8 +1,48 @@
 package sarankar.app.jetnewsapp.ui.screens.homescreen.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 
+@ExperimentalMaterial3Api
 @Composable
-fun LoadingContent(){
-
+fun LoadingContent(
+    empty: Boolean,
+    emptyContent: @Composable () -> Unit,
+    loading: Boolean,
+    onRefresh: () -> Unit,
+    content: @Composable () -> Unit,
+) {
+    val state = rememberPullToRefreshState().apply {
+        if(loading){
+            startRefresh()
+        }else{
+            endRefresh()
+        }
+    }
+    if(state.isRefreshing){
+        LaunchedEffect(true) {
+            onRefresh()
+            state.endRefresh()
+        }
+    }
+    if (empty) {
+        emptyContent()
+    } else {
+        Box(
+            modifier = Modifier.nestedScroll(state.nestedScrollConnection)
+        ) {
+            content()
+            PullToRefreshContainer(
+                state = state,
+                modifier =  Modifier.align(Alignment.TopCenter)
+            )
+        }
+    }
 }
