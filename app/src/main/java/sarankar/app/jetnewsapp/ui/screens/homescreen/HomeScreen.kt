6e172e77d76022ada6d95584dd688558e70bc9ae
pratham_srnk <pratham@appointy.com>
2024-04-22 12:38:04 +0530

    Adding homescreen

--- app/src/main/java/sarankar/app/jetnewsapp/ui/screens/homescreen/HomeScreen.kt
+++ app/src/main/java/sarankar/app/jetnewsapp/ui/screens/homescreen/HomeScreen.kt
@@ -1,7 +1,12 @@
 package sarankar.app.jetnewsapp.ui.screens.homescreen
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 
 @Composable
 fun HomeScreen(){
@@ -9,3 +14,25 @@ fun HomeScreen(){
 
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreenScaffold(
+    modifier: Modifier = Modifier,
+    showTopAppBar: Boolean,
+){
+    val topAppBarState = rememberTopAppBarState();
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior();
+    Scaffold(
+        topBar = {
+            if(showTopAppBar){
+                )
+            }
+        }
+    ) { innerPadding ->
+        val contentModifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+
+    }
+
+}
+
---
+++ app/src/main/java/sarankar/app/jetnewsapp/ui/screens/homescreen/components/LoadingContent.kt
@@ -0,0 +1,8 @@
+package sarankar.app.jetnewsapp.ui.screens.homescreen.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoadingContent(){
+
+}
